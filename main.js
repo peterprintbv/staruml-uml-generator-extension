382dfc08818a4d8583fe392195ebc54ff2fb486d
Eric Landheer <eric@peterprint.nl>
2023-06-28 16:24:07 +0200

    :rainbow: Refactor selectDirectory to searate function

--- main.js
+++ main.js
@@ -8,6 +8,31 @@ const UmlGenerator = require("./uml-generator");
  * @param {Object} options
  */
 async function handleGenerate (base, path, options) {
+    const directory = await selectDirectory();
+
+    if (directory === null) {
+        app.toast.warning('No directory selected. Aborted generating docs.');
+        return;
+    }
+
+    app.toast.info('Starting documentation generation...');
+
+    try {
+        const files = FileReader.getFilesForPath(directory);
+        const groupFiles = FileReader.groupFilesByDirectory(files);
+        UmlGenerator.generateDocs(groupFiles);
+    } catch (exception) {
+        app.toast.error('Something went wrong while generating documentation! Exception: ' + exception);
+    }
+
+    app.toast.info('Documentation generated successfully!');
+}
+
+/**
+ * Select the directory using the app open dialog, passing the openDirectory to enforce directory selection.
+ * @returns {String|null}
+ */
+async function selectDirectory() {
     const directories = await app.dialogs.showOpenDialog(
         'Select module to generate docs for',
         undefined,
@@ -20,21 +45,10 @@ async function handleGenerate (base, path, options) {
     );
 
     if (directories === undefined || directories.length === 0) {
-        app.toast.warning('Aborted generation.');
-        return;
+        return null;
     }
 
-    app.toast.info('Starting documentation generation...');
-
-    try {
-        const files = FileReader.getFilesForPath(directories[0]);
-        const groupFiles = FileReader.groupFilesByDirectory(files);
-        UmlGenerator.generateDocs(groupFiles);
-    } catch (exception) {
-        app.toast.error('Something went wrong while generating documentation! Exception: ' + exception);
-    }
-
-    app.toast.info('Documentation generated successfully!');
+    return directories[0];
 }
 
 function init () {
